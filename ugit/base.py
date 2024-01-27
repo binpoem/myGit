3d1a2a707af0a545f8bea75d8561279965842b69
binpoem <binpoem@gmail.com>
2024-01-27 15:18:40 +0800

    tag: Implement CLI command

--- ugit/base.py
+++ ugit/base.py
@@ -102,6 +102,11 @@ def checkout(oid):
     data.set_HEAD(oid)
 
 
+def create_tag(name, oid):
+    # TODO Actually create the tag
+    pass
+
+
 Commit = namedtuple("Commit", ["tree", "parent", "message"])
 
 
